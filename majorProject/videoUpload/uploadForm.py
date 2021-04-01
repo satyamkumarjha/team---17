d70d65115b6a9c42ae50ecafb7c78c5239702db1
Satyam Kumar Jha <satyamjhakumar@gmail.com>
2021-04-01 18:40:29 +0530

    added quiz, dashboard and modified course

--- majorProject/videoUpload/uploadForm.py
+++ majorProject/videoUpload/uploadForm.py
@@ -1,6 +1,2 @@
 from django import forms
 
-class UploadFileForm(forms.Form):
-    course = forms.CharField(max_length=50)
-    FileName = forms.CharField(max_length=50)
-    file = forms.FileField()
